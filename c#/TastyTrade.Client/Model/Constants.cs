a2e42ad254c99c2f949273c7b1f8e1b274eccf90
VeritaXCodex <verita.codex@gmail.com>
2024-08-05 03:33:39 +0200

    .

--- c#/TastyTrade.Client/Model/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TastyTrade.Client.Model;
-
-public static class Constants
-{
-    public const string ContentType = "application/json";
-    public const string Accept = "application/json";
-}
