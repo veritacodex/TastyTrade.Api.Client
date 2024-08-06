a77946d4e20408a1fd8172ca1420f9421e876ae0
VeritaXCodex <verita.codex@gmail.com>
2024-08-06 05:11:15 +0200

    .

--- TastyTrade.Client.Examples/Program.cs
+++ TastyTrade.Client.Examples/Program.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.IO;
-using System.Security.Cryptography;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
-using DxFeed.Graal.Net;
-using DxFeed.Graal.Net.Api;
-using DxFeed.Graal.Net.Events.Market;
-using Newtonsoft.Json;
-using TastyTrade.Client.Model.Request;
 
 namespace TastyTrade.Client.Examples;
 
